5340f3c506a1f16bc9bb41920a1f8795c429846d
Sasa Brankovic <sasa@hakeraj.com>
2013-09-29 14:50:07 +0200

    Api service

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,21 @@
 var hackular = angular.module('hackular', [])
 
 hackular.config(function($routeProvider) {
-    $routeProvider.when("/", { templateUrl: "partials/feed.html", controller: "FeedCtrl" });
+    $routeProvider.when("/", { templateUrl: "partials/feed.html", controller: "FeedCtrl" })
     $routeProvider.otherwise({ redirectTo: "/" })
 })
 
-hackular.controller("FeedCtrl", function() {
+hackular.factory("api", function($http) {        
+    return {
+        news: function() {
+            var url = "http://hnapp.com/api/items/json/8f370f98f4cc52e4da0640c07bd4d850"
+            return $http.get(url)
+        }
+	}
+})
+
+hackular.controller("FeedCtrl", function(api) {
+    api.news().success(function(feed) {
+        console.debug(feed)
+    })
 })

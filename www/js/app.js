bf1b64c380f0211df0bd1ead5429b118bce0bbc5
Sasa Brankovic <sasa@hakeraj.com>
2013-09-29 15:12:36 +0200

    Api service - article method

--- www/js/app.js
+++ www/js/app.js
@@ -11,6 +11,16 @@ hackular.factory("api", function($http) {
         news: function() {
             var url = "http://hnapp.com/api/items/json/8f370f98f4cc52e4da0640c07bd4d850"
             return $http.get(url)
+        },
+        article: function(articleUrl) {    
+            var url = "https://readability.com/api/content/v1/parser"
+            var token = "45254f581ce504ab2e9919c907449d01067892da"
+            return $http.get(url, { 
+                params: {
+                    url: articleUrl,
+                    token: token
+                }
+            }) 
         }
 	}
 })
@@ -25,5 +35,10 @@ hackular.controller("FeedCtrl", function($scope, $location, api) {
     }
 })
 
-hackular.controller("ArticleCtrl", function() {
+hackular.controller("ArticleCtrl", function($scope, $routeParams, api) {
+    var url = decodeURIComponent($routeParams.url)
+    api.article(url).success(function(article) {
+        console.debug(article)
+    })
+    
 })
